02be803f8275c812d7e92bfd7c49addf03c77673
Arslanoov <rasularslanoov@gmail.com>
2021-04-11 20:46:45 +0500

    feat: Added cards grid

---
+++ app/components/common/post/cards-grid/CardsGrid.vue
@@ -0,0 +1,129 @@
+<template lang="pug">
+  .cards-grid
+    .cards-grid__item(v-for="(post, index) in posts", key=`post.id`)
+      img(v-if=`post.image`, :src=`post.image`, alt="").cards-grid__image
+      .cards-grid__content(:class=`index === posts.length - 1 ? "cards-grid__content_resettled" : ""`)
+        .cards-grid__element.cards-grid__title(:class=`index === 0 ? "cards-grid__title_underlined" : ""`) {{ post.title }}
+        .cards-grid__element.cards-grid__date {{ post.createdAt }}
+        .cards-grid__element.cards-grid__short(v-if=`post.short`) {{ post.short }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api"
+
+export default defineComponent({
+  props: {
+    posts: {
+      /* TODO: Array<PostInterface> */
+      type: Array,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.cards-grid {
+  grid-column: col-start 1 / col-end 12;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+
+  gap: 3rem;
+
+  margin: 5rem 0;
+
+  &__item,
+  &__image {
+    &:not(:last-of-type) {
+      border-radius: 1rem;
+    }
+  }
+
+  &__image {
+    width: 100%;
+  }
+
+  &__image,
+  &__title {
+    .pointer-on-hover();
+  }
+
+  &__item {
+    &:not(:last-of-type) {
+      box-shadow: 0 0 2rem rgba(215, 215, 215, .35);
+    }
+
+    &:first-child {
+      grid-column: 1 / span 1;
+      grid-row: 1 / -1;
+    }
+
+    &:nth-of-type(2) {
+      grid-column: 2 / span 1;
+      grid-row: 1 / span 2;
+    }
+
+    &:nth-of-type(3) {
+      grid-column: 3 / span 1;
+      grid-row: 1 / span 2;
+    }
+
+    &:last-of-type {
+      grid-column: 2 / span 2;
+      grid-row: 3 / -1;
+
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      & > * {
+        &:first-of-type {
+          margin-right: 2.5rem;
+        }
+      }
+    }
+  }
+
+  &__content {
+    padding: 2.5rem;
+
+    &_resettled {
+      padding: 0;
+    }
+  }
+
+  &__element {
+    &:not(:last-of-type) {
+      margin-bottom: 1rem;
+    }
+  }
+
+  &__title {
+    font-size: 2rem;
+    font-weight: 500;
+
+    line-height: 2.8rem;
+
+    color: #0b0f18;
+
+    &_underlined {
+      text-decoration: underline;
+    }
+  }
+
+  &__date,
+  &__short {
+    font-size: 1.4rem;
+  }
+
+  &__date {
+    color: #e68c8c;
+  }
+
+  &__short {
+    color: rgba(#0b0f18, .8);
+  }
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,22 @@
 <template lang="pug">
-  .container.home
-    MainCard(:post=`post`).card
+  .home
+    .container.main-card-container
+      MainCard(:post=`post`).main-card-container__card
+
+    .container
+      CardsGrid(:posts=`posts`)
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "@vue/composition-api"
+import { defineComponent, reactive, ref } from "@vue/composition-api"
 
 import MainCard from "~/components/common/post/main-card/MainCard.vue"
+import CardsGrid from "~/components/common/post/cards-grid/CardsGrid.vue"
 
 export default defineComponent({
   components: {
-    MainCard
+    MainCard,
+    CardsGrid
   },
   setup() {
     const post = reactive({
@@ -26,38 +32,67 @@ export default defineComponent({
         "splashing around in the water and exploring your limits. "
     })
 
+    const posts = ref([
+      {
+        id: 1,
+        createdAt: "January 02, 2020",
+        title: "It's now easier to get between the US and Cape Town ",
+        image: "/images/mock/blog/posts/1.jpg"
+      },
+      {
+        id: 2,
+        createdAt: "January 02, 2020",
+        title: "The best Vietnam islands to visit",
+      },
+      {
+        id: 3,
+        createdAt: "January 02, 2020",
+        title: "12 Things to do in Barcelona in September",
+      },
+      {
+        id: 4,
+        createdAt: "January 02, 2020",
+        title: "5 Things to do in Barcelona This Summer That Are Not in Your Guide Book",
+        image: "/images/mock/blog/posts/2.jpg",
+        short: "The plan in the beginning was always to leave after a year." +
+          " I’ve spent my share of summers here, as I tend to leave vacations " +
+          "for the winter. "
+      }
+    ])
+
     return {
-      post
+      post,
+      posts
     }
   }
 })
 </script>
 
 <style lang="less" scoped>
-.home {
+.main-card-container {
   height: @main-section-height;
 
   align-content: center;
 
-  background-color: #aeaeae;
+  background-color: @home-main-card-background;
+
+  &__card {
+    grid-column: col-start 1 / span 8;
+
+    .respond(@sizes[tablet-land], {
+      grid-column: col-start 1 / span 10;
+    }, @without-screen);
+
+    .respond(@sizes[tablet], {
+      grid-column: col-start 1 / col-end 12;
+    }, @without-screen);
+  }
 
   .respond(@sizes[desktop-sm], {
     height: auto;
     min-height: @main-section-height;
 
     padding: 2rem 0;
-  }, @without-screen);
-}
-
-.card {
-  grid-column: col-start 1 / span 8;
-
-  .respond(@sizes[tablet-land], {
-    grid-column: col-start 1 / span 10;
-  }, @without-screen);
-
-  .respond(@sizes[tablet], {
-    grid-column: col-start 1 / col-end 12;
   }, @without-screen);
 }
 </style>
